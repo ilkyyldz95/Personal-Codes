712db53bde3835dfc5485cf79d89c975a069f32a
ilkyyldz95 <ilkyyldz95@gmail.com>
2017-10-10 18:55:28 -0400

    backup

--- ComparisonCNNupd/abs_comp_net_train.py
+++ ComparisonCNNupd/abs_comp_net_train.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 from __future__ import print_function
-from keras.models import Model, model_from_json
 from keras.layers import Input, Lambda
 from keras.optimizers import RMSprop
 from keras import backend as K
+from ComparisonCNNupd.googlenet import *
 
 '''Imports F trained with absolute labels and trains with comparison labels'''
 
@@ -15,7 +15,6 @@ def BTPred(scalars):
     s2 = scalars[1]
     return s1 - s2
 
-
 def BTLoss(y_true, y_pred):
     """
     Negative log likelihood of Bradley-Terry Penalty, to be minimized. y = beta.*x
@@ -24,25 +23,25 @@ def BTLoss(y_true, y_pred):
     return K.log(1 + exponent)
 
 # INITIALIZE PARAMETERS
-# size of image
-input_dim = (3,224,224)
+hid_layer_dim = 1 #score
+input_shape = (3,224,224)
+no_of_images = 196
+no_of_test_images = 64
+no_of_features = 1024
 epochs = 10
-batch_size = 1
+batch_size = 32
 
 # LOAD TRAIN DATA FOR COMPARISON LABELS: tr_pairs, tr_y
 
 # LOAD JAMES' NETWORK FOR F, call abs_net
-json_file = open('abs_label_model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-abs_net = model_from_json(loaded_model_json)
-abs_net.load_weights("abs_label_model.h5")
+abs_net = create_googlenet(no_classes=hid_layer_dim, no_features=no_of_features)
+abs_net.load_weights("abs_label_F.h5")
 print("F loaded")
 
 # CREATE TWIN NETWORKS
 # because we re-use the same instance `base_network`, the weights of the network will be shared across the two branches
-input_a = Input(shape=(input_dim,))
-input_b = Input(shape=(input_dim,))
+input_a = Input(shape=(input_shape,))
+input_b = Input(shape=(input_shape,))
 
 processed_a = abs_net(input_a)
 processed_b = abs_net(input_b)
--- ComparisonCNNupd/abs_net_train_g.py
+++ ComparisonCNNupd/abs_net_train_g.py
@@ -11,6 +11,12 @@ import glob
 from ComparisonCNNupd.googlenet import *
 
 '''Trains 3 different G  with absolute labels'''
+def mySoftMax(inputs):
+    out_norm = K.exp(inputs[0])/K.sum(K.exp(inputs))
+    out_pre = K.exp(inputs[1]) / K.sum(K.exp(inputs))
+    out_plus = K.exp(inputs[2]) / K.sum(K.exp(inputs))
+    return K.stack([out_norm, out_pre, out_plus])
+
 def create_network(F, hid_layer_dim, input_shape):
     ''' F&G concatenated
     3 parallel networks, with the same F and different Gs'''
@@ -39,10 +45,9 @@ def create_network(F, hid_layer_dim, input_shape):
     processed_pre = net_pre_plus(Input(shape=input_shape))
     processed_plus = net_plus(Input(shape=input_shape))
 
-    loss3_classifier = Dense(3, input_shape=(3,), name='loss3/classifier')([processed_norm, processed_pre, processed_plus])
-    out_layer = Activation('softmax', name='prob')(loss3_classifier)
+    concat_abs_net = Model(Input(shape=input_shape), [processed_norm, processed_pre, processed_plus])
+    concat_abs_net.add(Activation('softmax', input_shape = (3,)))
 
-    concat_abs_net = Model(Input(shape=input_shape), out_layer)
     return concat_abs_net
 
 def absLoss(y_true, y_pred):
@@ -61,10 +66,10 @@ def absLoss(y_true, y_pred):
     return -K.log(diff)
 
 # INITIALIZE PARAMETERS
-hid_layer_dim = 1 # 1 for James' code: score
+hid_layer_dim = 1 #score
 input_shape = (3,224,224)
 no_of_images = 196
-no_of_test_images = 76
+no_of_test_images = 64
 no_of_features = 1024
 epochs = 10
 batch_size = 32
@@ -73,24 +78,28 @@ optimizer = 'sgd'
 
 # LOAD TEST DATA FOR ABSOLUTE LABELS
 images = np.zeros((no_of_images,3,224,224))
+shuff_images = np.zeros((no_of_images,3,224,224))
+abs_labels = np.zeros((no_of_images,))
+shuff_abs_labels = np.zeros((no_of_images,))
 count = 0
+
 for filename in glob.glob('../ComparisonCNNupd/vessels/*.png'):
     im = np.asarray(Image.open(filename).convert('L'))
     images[count][:][:][:] = im[128:352,208:432]
     count += 1
-abs_labels = np.zeros((no_of_images,))
 # 0 for normal, 1 for pp, 2 for p
 abs_labels[:31] = 1
 abs_labels[31:48] = 2
 abs_labels[101:135] = 1
 abs_labels[135:149] = 2
+
 # shuffle the data
 p = np.random.permutation(no_of_images)
 for im in range(no_of_images):
-    images[im][:][:][:] = images[p[im]][:][:][:]
-    abs_labels[im] = abs_labels[p[im]]
-test_im = images[:no_of_test_images][:][:][:]
-test_label = abs_labels[:no_of_test_images]
+    shuff_images[im][:][:][:] = images[p[im]][:][:][:]
+    shuff_abs_labels[im] = abs_labels[p[im]]
+test_im = shuff_images[:no_of_test_images][:][:][:]
+test_label = shuff_abs_labels[:no_of_test_images]
 
 # LOAD JAMES' NETWORK FOR F, call abs_net
 abs_net = create_googlenet(no_classes=hid_layer_dim, no_features=no_of_features)
@@ -101,3 +110,7 @@ concat_abs_net = create_network(abs_net, hid_layer_dim, input_shape)
 # Train all models with corresponding images
 concat_abs_net.compile(loss=loss, optimizer=optimizer)
 concat_abs_net.fit(test_im, test_label, batch_size=batch_size, epochs=epochs)
+
+# Save weights for F
+concat_abs_net.layers[0].save_weights("abs_label_F.h5")
+print("Saved model to disk")
